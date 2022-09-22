d0dab398e8137b3ed2573986f050267cae819822
EdwardPooh <69804800+EdwardPooh@users.noreply.github.com>
2022-09-22 16:57:30 +0800

    Delete Douzero_Resnet directory

--- Douzero_Resnet/train.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from douzero.dmc import parser, train
-
-if __name__ == '__main__':
-    flags = parser.parse_args()
-    os.environ["CUDA_VISIBLE_DEVICES"] = flags.gpu_devices
-    train(flags)
